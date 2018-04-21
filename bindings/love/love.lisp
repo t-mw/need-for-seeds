ffb7740ff65667da8e6d06738418d0385754e1a1
Tobias Mansfield-Williams <contact@tmwhere.com>
2018-04-21 15:18:24 +0200

    First physics

--- bindings/love/love.lisp
+++ bindings/love/love.lisp
@@ -3,7 +3,7 @@
 (defmacro defevent (ev args &body)
   (let [(sm (gensym))]
     (if (list? ev)
-        `(let [(,sm (lambda ,(append '(self) args) ,@body))]
+        `(let [(,sm (lambda ,(append '(_) args) ,@body))]
            (.<! ,(nth ev 1) ,(nth ev 2), sm))
         `(let* [(,sm (lambda ,args ,@body))]
            (.<! *love-events* ,ev ,sm)))))
--- main.lisp
+++ main.lisp
@@ -1,19 +1,33 @@
 ; -*- mode: scheme -*-
+(import core/demand (demand))
 (import core/method (debug))
+(import data/struct (defstruct))
 (import hump/gamestate)
 (import lua/math (cos sin))
-(import love/love (defevent))
 (import love/graphics)
+(import love/keyboard)
+(import love/love (defevent))
 (import love/timer)
 (import love-repl)
 (import lume)
-(import lurker)
 (import math/vector ())
+(import windfield)
+
+(defun rotate (v r)
+  (let ([c (cos r)]
+        [s (sin r)])
+    (vector (- (* c (vector-item v 1)) (* s (vector-item v 2)))
+            (+ (* s (vector-item v 1)) (* c (vector-item v 2))))))
 
 (define state { :foo "bar" })
+(defstruct physics
+  (fields (mutable world)
+          (mutable harvester-main)
+          (mutable harvester-head)))
+(define physics (make-physics))
 
 (define gamestate-start {})
-(define gamestate-test {})
+(define gamestate-main {})
 (define gamestate-repl {})
 
 (define resources-test-image :mutable nil)
@@ -22,52 +36,65 @@
          (love/graphics/new-image "assets/test.png")))
 
 (defun configure-packages ()
-  (.<! lurker/*lurker* :preswap
-       (lambda ()
-         (load-resources)))
   (.<! love-repl/*love-repl* :screenshot true)
   (.<! love-repl/*love-repl* :toggle_key "ignore"))
 
-(defevent :update (dt)
-  (lurker/update))
-
 (defevent :load ()
-  (load-resources)
   (configure-packages)
   (love-repl/initialize)
+  (load-resources)
+
   (hump/gamestate/register-events)
-  (hump/gamestate/switch gamestate-start))
+  (hump/gamestate/switch gamestate-main))
+
+(defevent :update (dt))
 
 (defevent :keypressed (key code)
   (when (and (not (love-repl/toggled)) (= key "f8"))
         (hump/gamestate/push gamestate-repl)))
 
-(defevent (gamestate-start :draw) ()
-  (let ([msg "Hello world! Hello world! Hello world! \
-              Hello world! Hello world! Hello world! Hello world! \
-              Hello world! Hello world! Hello world! Hello world! \
-              Hello world! Hello world! Hello world! Hello world! \
-              Hello world! Hello world! Hello world! Hello world! \
-              Hello world! Hello world! Hello world! Hello world! \
-              Hello world! Hello world! Hello world! Hello world! \
-              Hello world! Hello world! Hello world! Hello world!"]
-        [width (love/graphics/get-width)]
-        [height (love/graphics/get-height)]
-        [time (love/timer/get-time)])
-    (love/graphics/print (lume/wordwrap msg 40) 400 300)
-    (let ([x (+ (/ width 2) (* 100 (cos time)))]
-          [y (+ (/ height 2) (* 100 (sin time)))])
-      (love/graphics/draw resources-test-image x y))))
+(defevent (gamestate-start :draw) ())
 
 (defevent (gamestate-start :keypressed) (key code)
   (case key
-    ["space" (hump/gamestate/switch gamestate-test)]
+    ["space" (hump/gamestate/switch gamestate-main)]
     [?default]))
 
-(defevent (gamestate-test :draw) ()
-  (love/graphics/print "foo" 400 300))
+(defevent (gamestate-main :enter) ()
+  (let* ([world (windfield/new-world 0 0 true)]
+         [harvester-main (self world :newRectangleCollider -40 0 80 80)]
+         [harvester-head (self world :newRectangleCollider -100 100 200 10)])
+        (set-physics-world! physics world)
+        (set-physics-harvester-main! physics harvester-main)
+        (set-physics-harvester-head! physics harvester-head)
+        (self harvester-head :applyAngularImpulse 5000)
+        (with (joint (self world :addJoint "RevoluteJoint" harvester-main harvester-head 0 80 true))
+              (self joint :setLimitsEnabled true)
+              (self joint :setLowerLimit -0.3)
+              (self joint :setUpperLimit 0.3))))
 
-(defevent (gamestate-test :keypressed) (key code)
+(defevent (gamestate-main :update) (dt)
+  (self (physics-world physics) :update dt)
+  (when (love/keyboard/is-down "up")
+        (let* ([angle (self (physics-harvester-main physics) :getAngle)]
+               [v (* 1000 (rotate (vector 0 1) angle))])
+          (self (physics-harvester-main physics) :applyForce (vector-item v 1) (vector-item v 2))))
+  (when (love/keyboard/is-down "left")
+        (self (physics-harvester-main physics) :applyAngularImpulse 1000))
+  (when (love/keyboard/is-down "right")
+        (self (physics-harvester-main physics) :applyAngularImpulse -1000)))
+
+(defevent (gamestate-main :draw) ()
+  (let ([width (love/graphics/get-width)]
+        [height (love/graphics/get-height)])
+    ;; (0 0) is at bottom-middle of screen.
+    ;; x +ve is right, y +ve is up.
+    (love/graphics/translate (/ width 2) height)
+    (love/graphics/scale 1 -1)
+    (self (physics-world physics) :draw)
+    (love/graphics/origin)))
+
+(defevent (gamestate-main :keypressed) (key code)
   (case key
     ["space" (hump/gamestate/switch gamestate-start)]
     [?default]))
